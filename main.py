b93b2f46013bc21e16b84ecf7a942aa035e62d0f
Sharmistha Shaw <sharmisthashaw3@gmail.com>
2023-03-13 03:56:43 +0530

    remove email function

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, EmailField
 from wtforms.validators import DataRequired
 import os
-import smtplib
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("AppSecretKey")
@@ -21,22 +20,7 @@ class ContactForm(FlaskForm):
 @app.route('/', methods=['GET', 'POST'])
 def home():
     form = ContactForm()
-    if form.validate_on_submit():
-        name = form.name.data
-        email = form.email.data
-        message = form.message.data
-        my_email = os.getenv("MyEmail")
-        password = os.getenv("MyPassword")
-        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-            connection.starttls()
-            connection.login(user=my_email, password=password)
 
-            connection.sendmail(
-                from_addr=my_email,
-                to_addrs="sharmisthashaw3@gmail.com",
-                msg=f"Subject:Feedback from My Portfolio \n\nName = {name}\nEmail = {email}\nMessage = {message}"
-            )
-        return redirect(url_for("home"))
     return render_template("index.html", form=form)
 
 
